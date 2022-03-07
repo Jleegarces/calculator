0d8c82b53886f29e3e276e8ad798c2a78249e33e
Jonnathan Garces <jonnathangarces@Jonnathans-MacBook-Pro.local>
2022-03-07 13:30:50 -0500

    done

---
+++ main.js
@@ -0,0 +1,26 @@
+let num1 = parseInt(prompt("enter a number: "));
+let num2 = parseInt(prompt("enter a second number: "));
+
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+
+
+function add () {
+  addSum = num1 + num2;
+  document.getElementById("sum-el").textContent = addSum;
+};
+
+function subtract () {
+  subSum = num1 - num2;
+  document.getElementById("sum-el").textContent = subSum;
+};
+
+function divide () {
+  divSum = num1 / num2;
+  document.getElementById("sum-el").textContent = divSum;
+};
+
+function multiply () {
+  mulSum = num1 * num2;
+  document.getElementById("sum-el").textContent = mulSum;
+};
